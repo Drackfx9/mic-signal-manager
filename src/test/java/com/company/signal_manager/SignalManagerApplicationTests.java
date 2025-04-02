a5e3d7dbc0a5616d080f7d075cad1d138a92a393
Erick T.S <ericktoaquiza@gmail.com>
2025-04-02 21:34:47 +0200

    first commit

---
+++ src/test/java/com/company/signal_manager/SignalManagerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.company.signal_manager;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SignalManagerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
